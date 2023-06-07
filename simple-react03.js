ebf7805993a951ce43eb3d835b650168dfd6344e
jswan <jswannieman@gmail.com>
2023-06-07 14:43:41 +1000

    Commit simple-react03 Javascript Component

---
+++ simple-react03.js
@@ -0,0 +1,30 @@
+const Page = () => {
+  const changeThemAll = () => {
+    document.querySelectorAll(".liman").forEach((boelie) => {
+      boelie.style.backgroundColor = "pink";
+      boelie.style.color = "black";
+    });
+  };
+  return (
+    <nav>
+      <div className="top">
+        <h1>Swannie's Clothing Company</h1>
+        <ul>
+          <div className="li__bunch">
+            <li className="liman" onClick={changeThemAll}>
+              Pricing
+            </li>
+            <li className="liman" onClick={changeThemAll}>
+              About
+            </li>
+            <li className="liman" onClick={changeThemAll}>
+              Contact
+            </li>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+ReactDOM.render(<Page />, document.getElementById("root"));
