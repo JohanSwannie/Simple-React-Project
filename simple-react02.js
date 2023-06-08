292b2313be6bd73aebab8b08281aa902fc82b50d
jswan <jswannieman@gmail.com>
2023-06-08 10:13:41 +1000

    Delete unnecessary components

--- simple-react02.js
+++
@@ -1,6 +0,0 @@
-const diffy = document.getElementById("root");
-
-const heady = document.createElement("h1");
-heady.textContent = "Johan is 'n document object module monster";
-heady.className = "Johan";
-diffy.appendChild(heady);
